b108041f9c758a7018b9dd0679cbbc23c8d6d001
XE2LDL95 <xe2ldl95@gmail.com>
2026-07-17 12:05:20 -0600

    feat: physical keyboard support (F1-F12 macros, Tab/Enter, heard nav)

--- js/app.js
+++ js/app.js
@@ -199,6 +199,9 @@ function init() {
             });
         }
     } catch (_) {}
+
+    // Keyboard shortcuts for text inputs
+    initKeyboardHandlers();
 }
 
 function toggleBluetoothFields() {
